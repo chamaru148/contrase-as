acea20a635c43386436d8959ba13e12de5a7e617
chamaru148 <jleoortega2008@gmail.com>
2024-12-13 10:37:14 -0500

    primer envio

---
+++ main.py
@@ -0,0 +1,10 @@
+import random as r, string as s
+
+elemen = s.ascii_letters + s.ascii_lowercase + s.ascii_uppercase + s.digits + s.punctuation
+pasword = ""
+largo = int(input("cuantos caracteres tendra la contraseña: "))
+
+for i in range (largo):
+    pasword += r.choice(elemen)
+
+print(pasword)
